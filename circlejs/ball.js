ef750798ca7dd9d20d55ee01a3d71a28f52ccbd7
MS1098 <mayurshelar1098@gmail.com>
2022-07-11 14:16:44 +0530

    gravity

--- circlejs/ball.js
+++ circlejs/ball.js
@@ -1,39 +1,157 @@
-const colors = ["#3CC157", "#2AA7FF", "#1B1B1B", "#FCBC0F", "#F85F36"];
-const numBalls = 50;
-var balls = [];
+// const colors = ["#3CC157", "#2AA7FF", "#1B1B1B", "#FCBC0F", "#F85F36"];
+// const numBalls = 50;
+// var balls = [];
 
-for (let i = 0; i < numBalls; i++) {
-  let ball = document.createElement("div");
-  ball.classList.add("ball");
-  ball.style.background = colors[Math.floor(Math.random() * colors.length)];
-  ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
-  ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
-  ball.style.transform = `scale(${Math.random()})`;
-  ball.style.width = "40px";
-  ball.style.height = "40px";
+// for (let i = 0; i < numBalls; i++) {
+//   let ball = document.createElement("div");
+//   ball.classList.add("ball");
+//   ball.style.background = colors[Math.floor(Math.random() * colors.length)];
+//   ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
+//   ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
+//   ball.style.transform = `scale(${Math.random()})`;
+//   ball.style.width = "40px";
+//   ball.style.height = "40px";
   
-  balls.push(ball);
-  document.getElementById("circle").append(ball);
-}
+//   balls.push(ball);
+//   document.getElementById("circle").append(ball);
+// }
 
-balls.forEach((el, i, ra) => {
-  let to = {
-    x: Math.random() * (i % 2 === 0 ? -11 : 11),
-    y: Math.random() * 12
-  };
-  let anim = el.animate(
-    [
-      { transform: "translate(0, 0)" },
-      { transform: `translate(${to.x}rem, ${to.y}rem)` }
-    ],
-    {
-      duration: (Math.random() + 1) * 2000, // random duration
-      direction: "alternate",
-      fill: "both",
-      iterations: Infinity,
-      easing: "ease-in-out"
-    }
-  );
+// balls.forEach((el, i, ra) => {
+//   let to = {
+//     x: Math.random() * (i % 2 === 0 ? -11 : 11),
+//     y: Math.random() * 12
+//   };
+//   let anim = el.animate(
+//     [
+//       { transform: "translate(0, 0)" },
+//       { transform: `translate(${to.x}rem, ${to.y}rem)` }
+//     ],
+//     {
+//       duration: (Math.random() + 1) * 2000, // random duration
+//       direction: "alternate",
+//       fill: "both",
+//       iterations: Infinity,
+//       easing: "ease-in-out"
+//     }
+//   );
+// });
+
+
+var canvas = document.querySelector('canvas');
+var c = canvas.getContext('2d');
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+
+// Variables
+var mouse = {
+	x: innerWidth / 2,
+	y: innerHeight / 2 
+};
+
+var colors = [
+	'#2185C5',
+	'#7ECEFD',
+	'#FFF6E5',
+	'#FF7F66'
+];
+
+var gravity = 0.2;
+var friction = 0.78;
+
+
+// Event Listeners
+addEventListener("mousemove", function(event) {
+	mouse.x = event.clientX;
+	mouse.y = event.clientY;
+});
+
+addEventListener("resize", function() {
+	canvas.width = innerWidth;	
+	canvas.height = innerHeight;
+  init();
+});
+
+addEventListener("click", function(event) {
+	init();
 });
 
 
+// Utility Functions
+function randomIntFromRange(min,max) {
+	return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function randomColor(colors) {
+	return colors[Math.floor(Math.random() * colors.length)];
+}
+
+
+// Objects
+function Ball(x, y, dx, dy, radius, color) {
+	this.x = x;
+	this.y = y;
+	this.dx = dx;
+	this.dy = dy;
+	this.radius = radius;
+	this.color = color;
+
+	this.update = function() {
+		if (this.y + this.radius + this.dy> canvas.height) {
+			this.dy = -this.dy;
+			this.dy = this.dy * friction;
+			this.dx = this.dx * friction;
+		} else {
+			this.dy += gravity;
+		}
+
+		if (this.x + this.radius >= canvas.width || this.x - this.radius <= 0) {
+			this.dx = -this.dx * friction;
+		}
+
+		this.x += this.dx;
+		this.y += this.dy;
+		this.draw();
+	};
+
+	this.draw = function() {
+		c.beginPath();
+		c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);	
+		c.fillStyle = this.color;
+		c.fill();
+		c.stroke();
+		c.closePath();
+	};
+}
+
+
+// Implementation
+var ballArray = [];
+
+function init() {
+	ballArray = [];
+
+	for (let i = 0; i < 200; i++) {
+		var radius = randomIntFromRange(8, 20);
+		var x = randomIntFromRange(radius, canvas.width - radius);
+		var y = randomIntFromRange(0, canvas.height - radius);
+		var dx = randomIntFromRange(-3, 3)
+		var dy = randomIntFromRange(-2, 2)
+	    ballArray.push(new Ball(x, y, dx, dy, radius, randomColor(colors)));
+	}
+}
+
+// Animation Loop
+function animate() {
+	requestAnimationFrame(animate);
+
+	c.clearRect(0, 0, canvas.width, canvas.height);
+
+	for (let i = 0; i < ballArray.length; i++) {
+		ballArray[i].update();
+	}
+}
+
+init();
+animate();
